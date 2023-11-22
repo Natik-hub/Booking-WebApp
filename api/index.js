66f8c7e52dbf6b3f7ec6473c89ddb260fe02735f
Natnael Kibru <nathnaelkibru@gmail.com>
2023-11-22 21:42:29 +0300

    Offlineupload_added

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
+
 require('dotenv').config();
 const app = express();
 
@@ -86,7 +89,7 @@ app.post('/logout', (req,res) => {
   res.cookie('token', '').json(true);
 });
 
-console.log({__dirname});
+
 app.post('/upload-by-link', async (req,res) => {
   const {link} = req.body;
   const newName = 'photo' + Date.now() + '.jpg';
@@ -95,6 +98,20 @@ app.post('/upload-by-link', async (req,res) => {
     dest: __dirname + '/uploads/' +newName,
   });
   res.json(newName);
+});
+
+const photosMiddleware = multer({dest:'uploads/'});
+app.post('/upload', photosMiddleware.array('photos', 100), (req,res) => {
+  const uploadedFiles = [];
+  for (let i = 0; i < req.files.length; i++) {
+    const {path,originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads' , ''));
+  }
+  res.json(uploadedFiles);
 });
 
 
